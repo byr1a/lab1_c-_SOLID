8e82010bebbbc7cc5e3978137b571543fc5c9a41
byr1a <vasa7433@gmail.com>
2024-09-26 01:41:56 +0700

    first commit

---
+++ ConsoleInput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+public class ConsoleInput : IUserInput
+{
+    public int GetInput()
+    {
+        return int.Parse(Console.ReadLine());
+    }
+}
